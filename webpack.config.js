c958d518587cdcded9032c83f725fd9c37153fd0
FrozenYogurtPuff <frozenyogurtpuff@gmail.com>
2021-10-31 19:09:08 +0800

    build(webpack): npm webpack script for example and main entrypoint

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,11 @@ const path = require('path')
 const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-  entry: path.join(__dirname, 'demo.js'),
-  mode: 'development',
-  watch: true,
-  devtool: 'source-map',
+  entry: path.join(__dirname, 'index.js'),
+  mode: 'production',
   output: {
     filename: 'main.js',
-    path: __dirname
+    path: path.join(__dirname, 'dist')
   },
   optimization: {
     minimizer: [new TerserPlugin({
