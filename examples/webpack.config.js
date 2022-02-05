35c7b5b927af3cfed3a8eec753b517f99f5e51e3
FrozenYogurtPuff <frozenyogurtpuff@gmail.com>
2022-02-05 17:27:40 +0800

    chore(ts-loader): import ts-loader in webpack.config.js

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,14 +1,25 @@
-const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, "demo.js"),
-  mode: "development",
+  entry: path.join(__dirname, 'demo.ts'),
+  mode: 'development',
   watch: true,
-  devtool: "source-map",
+  devtool: 'source-map',
   output: {
-    filename: "main.js",
+    filename: 'main.js',
     path: __dirname,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
   },
   optimization: {
     minimizer: [
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
-const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, "index.js"),
-  mode: "production",
+  entry: path.join(__dirname, 'src/index.ts'),
+  mode: 'production',
   output: {
-    filename: "main.js",
-    path: path.join(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.join(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
   },
   optimization: {
     minimizer: [
